7622eabe8fc9d3fe18abaeacec4f51f3459d09bb
CNYT <cuneyt.taskent@gmail.com>
2020-03-30 12:04:29 +0300

    RuntimeVCariables are added.

---
+++ Assets/Scripts/RuntimeVariables/AnimationVo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [Serializable]
+    public class AnimationVo
+    {
+        public string Name;
+
+        public float Duration;
+
+        public float KickFrame;
+
+        public Vector3 KickPosition;
+    }
+}
+
