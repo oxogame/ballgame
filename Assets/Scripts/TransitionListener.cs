abc90586762ae5b6686b886e3d7f1199dc0cd5ea
CNYT <cuneyt.taskent@gmail.com>
2020-03-30 19:04:40 +0300

    last commit for transaitionListener

--- Assets/Scripts/TransitionListener.cs
+++ Assets/Scripts/TransitionListener.cs
@@ -9,7 +9,7 @@ public class TransitionListener : MonoBehaviour
 
     private int nextAnimId;
 
-
+    // Animasyon Eventlerinden tetikleniyor.
     public void CheckEvent(int status)
     {
         currAnimId = status;
@@ -17,29 +17,41 @@ public class TransitionListener : MonoBehaviour
 
         Debug.Log("Checking Event----------- ");
         Debug.Log("Animation " + currAnimId + " --> " + nextAnimId);
+
+        this.GetComponent<Animator>().speed = 0;
+    }
+
+    [Button(ButtonSizes.Gigantic)]
+    public void Play()
+    {
+        this.GetComponent<Animator>().speed = 1;
     }
 
     [Button(ButtonSizes.Gigantic)]
     public void Play1()
     {
         this.GetComponent<Animator>().SetInteger("AnimId", 1);
+        this.GetComponent<Animator>().speed = 1;
     }
 
     [Button(ButtonSizes.Gigantic)]
     public void Play2()
     {
         this.GetComponent<Animator>().SetInteger("AnimId", 2);
+        this.GetComponent<Animator>().speed = 1;
     }
 
     [Button(ButtonSizes.Gigantic)]
     public void Play3()
     {
         this.GetComponent<Animator>().SetInteger("AnimId", 3);
+        this.GetComponent<Animator>().speed = 1;
     }
 
     [Button(ButtonSizes.Gigantic)]
     public void Play4()
     {
         this.GetComponent<Animator>().SetInteger("AnimId", 4);
+        this.GetComponent<Animator>().speed = 1;
     }
 }
